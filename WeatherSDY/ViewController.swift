1f2fc8f79e2b7eb480c03d6970d96f465a3dc19f
Duyong Shim <shimdy1013@naver.com>
2022-07-02 03:21:21 +0900

    Initial Commit

---
+++ WeatherSDY/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherSDY
+//
+//  Created by 심두용 on 2022/07/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
