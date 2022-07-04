9d91a5214851638a6778d8f18bf12b5cbdc05ba6
Duyong Shim <shimdy1013@naver.com>
2022-07-04 16:49:06 +0900

    Step1

--- WeatherSDY/ViewController.swift
+++ WeatherSDY/ViewController.swift
@@ -13,7 +13,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    private var apiKey: String {
+        get {
+            // 생성한 .plist 파일 경로 불러오기
+            guard let filePath = Bundle.main.path(forResource: "APIKey", ofType: "plist") else {
+                fatalError("Couldn't find file 'APIKey.plist'.")
+            }
+            
+            // .plist를 딕셔너리로 받아오기
+            let plist = NSDictionary(contentsOfFile: filePath)
+            
+            // 딕셔너리에서 값 찾기
+            guard let value = plist?.object(forKey: "OPENWEATHERMAP_KEY") as? String else {
+                fatalError("Couldn't find key 'OPENWEATHERMAP_KEY' in 'KeyList.plist'.")
+            }
+            return value
+        }
+    }
 
 }
 
