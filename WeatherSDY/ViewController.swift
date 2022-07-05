95e3980a0f3bb2d144f83c6446b400f945757ad1
Duyong Shim <shimdy1013@naver.com>
2022-07-05 21:39:29 +0900

    Step2

--- WeatherSDY/ViewController.swift
+++ WeatherSDY/ViewController.swift
@@ -9,28 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var minTempLabel: UILabel!
+    @IBOutlet weak var maxTempLabel: UILabel!
+    
+    
+    // 받아온 데이터를 저장할 프로퍼티
+    var weather: Weather?
+    var main: Main?
+    var name: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
-    private var apiKey: String {
-        get {
-            // 생성한 .plist 파일 경로 불러오기
-            guard let filePath = Bundle.main.path(forResource: "APIKey", ofType: "plist") else {
-                fatalError("Couldn't find file 'APIKey.plist'.")
+
+        // data fetch
+        WeatherService().getWeather { result in
+            switch result {
+            case .success(let weatherResponse):
+                DispatchQueue.main.async {
+                    self.weather = weatherResponse.weather.first
+                    self.main = weatherResponse.main
+                    self.name = weatherResponse.name
+                    self.setWeatherUI()
+                }
+            case .failure(_ ):
+                print("error")
             }
-            
-            // .plist를 딕셔너리로 받아오기
-            let plist = NSDictionary(contentsOfFile: filePath)
-            
-            // 딕셔너리에서 값 찾기
-            guard let value = plist?.object(forKey: "OPENWEATHERMAP_KEY") as? String else {
-                fatalError("Couldn't find key 'OPENWEATHERMAP_KEY' in 'KeyList.plist'.")
-            }
-            return value
         }
     }
-
+    
+    private func setWeatherUI() {
+        let url = URL(string: "https://openweathermap.org/img/wn/\(self.weather?.icon ?? "00")@2x.png")
+        let data = try? Data(contentsOf: url!)
+        if let data = data {
+            iconImageView.image = UIImage(data: data)
+        }
+        tempLabel.text = "\(main!.temp)"
+        maxTempLabel.text = "\(main!.temp_max)"
+        minTempLabel.text = "\(main!.temp_min)"
+    }
 }
 
